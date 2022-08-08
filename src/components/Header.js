76786f2765c1e4208c78e0f5523559a0e5873291
rohit <rohit.patelofficial007@gmail.com>
2022-08-08 02:31:43 -0800

    header-stage1 completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,48 @@
 import React from "react";
+import "./Header.css";
+import {
+  BookOpenIcon,
+  HomeIcon,
+  SearchIcon,
+  ShoppingBagIcon,
+} from "@heroicons/react/outline";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="logo">
+        <h3 className="logo_text">Logo</h3>
+      </div>
+      <div className="search">
+        <input type="text" placeholder="Enter your input" />
+        <SearchIcon className="search__icon" />
+      </div>
+      <div className="header_items">
+        <div className="items_option">
+          <HomeIcon className="home_icon" />
+          <span>Home</span>
+        </div>
+
+        <div className="items_option">
+          <BookOpenIcon className="home_icon" />
+          <span>Books Categories</span>
+        </div>
+
+        <div className="items_option">
+          <ShoppingBagIcon className="home_icon" />
+          <span>My List</span>
+        </div>
+      </div>
+      <div className="mobileView_search">
+        <SearchIcon />
+      </div>
+
+      {/* Login */}
+      <div className="login_option">
+        <button type="submit">Login</button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
